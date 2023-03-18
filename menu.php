5b385749b3ed9538e4daffdc8e0e73280e4a820b
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-03-18 17:24:24 -0400

    created menu bar and footer

---
+++ menu.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <title>Smart Customer Services</title>
+
+    <link rel="icon" href="icon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Electrolize">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    
+    <header>
+        <img class="menu-item" src="logo.png" alt="logo">
+
+        <button class="menu-item" onclick="window.open('#top', '_self')">Home</button>
+        <button class="menu-item" onclick="">About us</button>
+        <button class="menu-item" onclick="">Contact us</button>
+        <button class="menu-item" onclick="">Reviews</button>
+        <button class="menu-item" onclick="">Types of Services</button>
+        <button class="menu-item" onclick="">Shopping Cart</button>
+        <button class="menu-item" onclick="">Sign up</button>
+        <button class="menu-item" onclick="">Sign-in</button>
+    </header>
+
+    <footer>
+        <div class="footer-item">
+            Our Team:
+            <ul>
+                <li>Maher Muhtadi</li>
+                <li>Edward Sword</li>
+                <li>Arshpreet Singh</li>
+                <li>James Tan</li>
+            </ul>
+        </div>
+        
+        <div class="footer-item">
+            Contacts:
+            <ul>
+                <li>mmuhtadi@torontomu.ca</li>
+                <li>edward.sword@torontomu.ca</li>
+                <li>arshpreet.singh@torontomu.ca</li>
+                <li>russelljames.tan@torontomu.ca</li>
+            </ul>
+        </div>
+    </footer>
+
+</body>
+
+</html>
