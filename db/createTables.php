df6d154900f6ce778dc6cdd058fee28399d5d95f
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-03-19 13:10:52 -0400

    added messages

--- db/createTables.php
+++ db/createTables.php
@@ -1,5 +1,9 @@
 <?php
 include "connect.php";
+echo "Connected to database successfully.<br>";
 $commands = file_get_contents("tables.sql");
-mysqli_multi_query($connection, $commands);
+if (mysqli_multi_query($connection, $commands)) {
+    echo "All tables were created successfully.<br>";
+    echo "<a href='../home.php'>Homepage</a>";
+}
 ?>
