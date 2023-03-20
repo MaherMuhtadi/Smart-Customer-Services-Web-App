89291fc52a8ad39c0ce60202154516c2672206f3
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-03-20 01:13:58 -0400

    added img_path field to item table

--- db/tables.sql
+++ db/tables.sql
@@ -42,6 +42,7 @@ CREATE TABLE receipt (
 -- Stores all purchasable items
 CREATE TABLE item (
     item_id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    img_path VARCHAR(100),
     item_name VARCHAR(100),
     price DOUBLE,
     made_in VARCHAR(50),
