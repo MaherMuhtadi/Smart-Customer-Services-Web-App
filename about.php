a5aeba7345c658ba05b796405ed833193bb12299
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-03-19 23:57:35 -0400

    updated content and layout of about.php and contact.php

--- about.php
+++ about.php
@@ -7,10 +7,22 @@ function main() {
         "<main class='info'>
             <h1>Behind SCS</h1>
             <ul>
-                <li>Maher Muhtadi</li>
-                <li>Edward Sword</li>
-                <li>Arshpreet Singh</li>
-                <li>James Tan</li>
+                <li>
+                    <h2>Maher Muhtadi</h2>
+                    Tech Support
+                </li>
+                <li>
+                    <h2>Edward Sword</h2>
+                    Tech Support
+                </li>
+                <li>
+                    <h2>Arshpreet Singh</h2>
+                    Tech Support
+                </li>
+                <li>
+                    <h2>James Tan</h2>
+                    Tech Support
+                </li>
             </ul>
         </main>";
 }
--- contact.php
+++ contact.php
@@ -8,10 +8,22 @@ function main() {
             <h1>Need help?</h1>
             Our team is here to assist you! Contact us at:
             <ul>
-                <li>mmuhtadi@torontomu.ca</li>
-                <li>edward.sword@torontomu.ca</li>
-                <li>arshpreet.singh@torontomu.ca</li>
-                <li>russelljames.tan@torontomu.ca</li>
+                <li>
+                    <h2>Maher Muhtadi</h2>
+                    <a href='mailto:mmuhtadi@torontomu.ca'>mmuhtadi@torontomu.ca</a>
+                </li>
+                <li>
+                    <h2>Edward Sword</h2>
+                    <a href='mailto:edward.sword@torontomu.ca'>edward.sword@torontomu.ca</a>
+                </li>
+                <li>
+                    <h2>Arshpreet Singh</h2>
+                    <a href='mailto:arshpreet.singh@torontomu.ca'>arshpreet.singh@torontomu.ca</a>
+                </li>
+                <li>
+                    <h2>James Tan</h2>
+                    <a href='mailto:russelljames.tan@torontomu.ca'>russelljames.tan@torontomu.ca</a>
+                </li>
             </ul>
         </main>";
 }
