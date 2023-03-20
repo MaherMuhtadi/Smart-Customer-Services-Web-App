82c9cd8c692007270facb497fd18f98e4ee03a1d
Maher Muhtadi <m.muhtadi2002@gmail.com>
2023-03-20 01:13:41 -0400

    created shopping table

--- browse.php
+++ browse.php
@@ -5,6 +5,7 @@ if (!isset($_SESSION["user"])) {
     exit();
 }
 
+include "db/connect.php";
 include "layout.php";
 ?>
 
@@ -16,7 +17,33 @@ include "layout.php";
     <?php menuBar(); ?>
     
     <main>
-        <h1>Happy Shopping!</h1>        
+        <h1>Happy Shopping!</h1>
+        <table>
+            <tr>
+                <th>Image</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Origin</th>
+                <th>Department</th>
+                <th>Store</th>
+            </tr>
+            <?php
+            $result = mysqli_query($connection, "SELECT * FROM item");
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo
+                        "<tr><td><img src='".$row["img_path"]."'></td>"
+                        ."<td>".$row["item_id"]."</td>"
+                        ."<td>".$row["item_name"]."</td>"
+                        ."<td>".$row["price"]."</td>"
+                        ."<td>".$row["made_in"]."</td>"
+                        ."<td>".$row["department"]."</td>"
+                        ."<td>".$row["store_name"]."</td></tr>";
+                }
+            }
+            ?>
+        </table>
     </main>
     
     <?php footer(); ?>
